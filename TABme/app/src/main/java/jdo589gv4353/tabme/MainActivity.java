b6c7f4d45f710b8463422c39044b00fe2c70a7e9
Garner Vincent <garnervincent@gmail.com>
2016-03-25 17:29:32 -0500

    Fixed manifest file and got rid of title bar

--- TABme/app/src/main/java/jdo589gv4353/tabme/MainActivity.java
+++ TABme/app/src/main/java/jdo589gv4353/tabme/MainActivity.java
@@ -3,6 +3,7 @@ package jdo589gv4353.tabme;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 import java.util.logging.Handler;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         final android.os.Handler handler = new android.os.Handler();
