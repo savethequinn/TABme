094b78c9ec1a685e3a84190911b425a3f5ba5cf5
Garner Vincent <garnervincent@gmail.com>
2016-03-25 18:43:39 -0500

    start tab activity

--- TABme/app/src/main/java/jdo589gv4353/tabme/StartTab.java
+++ TABme/app/src/main/java/jdo589gv4353/tabme/StartTab.java
@@ -1,7 +1,18 @@
 package jdo589gv4353.tabme;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class StartTab extends AppCompatActivity {
 
@@ -9,5 +20,19 @@ public class StartTab extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_tab);
+
+        //generate list
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("item1");
+        list.add("item2");
+
+        //instantiate custom adapter
+        MyCustomAdapter adapter = new MyCustomAdapter(list, this);
+
+        //handle listview and assign adapter
+        ListView lView = (ListView) findViewById(R.id.my_listview);
+        lView.setAdapter(adapter);
     }
 }
+
+
