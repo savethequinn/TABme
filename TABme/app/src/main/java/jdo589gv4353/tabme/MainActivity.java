69517cba2efad7b69b10faacedf043271585e0d6
Garner Vincent <garnervincent@gmail.com>
2016-03-25 17:21:52 -0500

    Splash Screen Activity
    Goes to login after 3 seconds

--- TABme/app/src/main/java/jdo589gv4353/tabme/MainActivity.java
+++ TABme/app/src/main/java/jdo589gv4353/tabme/MainActivity.java
@@ -1,7 +1,10 @@
 package jdo589gv4353.tabme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.logging.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final android.os.Handler handler = new android.os.Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent mInHome = new Intent(MainActivity.this, Login.class);
+                MainActivity.this.startActivity(mInHome);
+                MainActivity.this.finish();
+            }
+        }, 3000);
     }
 }
